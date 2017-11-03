5c2d3abfd9fc30a261d726617a637e4e08c36b5d
Max Kueng <me@maxkueng.com>
2017-11-03 23:26:27 +0100

    Reduce lines of code per function

--- src/index.js
+++ src/index.js
@@ -6,37 +6,38 @@ function sleep(ms: number): Promise<void> {
   });
 }
 
-export default function wyt(turnsPerInterval: number, interval: number) {
+type State = {
+  lastReload: number;
+  availableTurns: number;
+};
+
+function reload(turnsPerInterval: number, interval: number, state: State) {
   const throughput = turnsPerInterval / interval;
+  const now = Date.now();
+  const reloadTime = Math.min(now - state.lastReload, interval);
+  const reloadedTurns = reloadTime * throughput;
+  const newAvalableTurns = state.availableTurns + reloadedTurns;
+
+  return {
+    lastReload: now,
+    availableTurns: Math.min(newAvalableTurns, turnsPerInterval),
+  };
+}
+
+export default function wyt(turnsPerInterval: number, interval: number) {
   const timePerTurn = interval / turnsPerInterval;
 
-  type State = {
-    lastReload: number;
-    availableTurns: number;
-  };
-
-  const state: State = {
+  let state: State = {
     lastReload: Date.now(),
     availableTurns: turnsPerInterval,
   };
-
-  function reload() {
-    const now = Date.now();
-    const timeDelta = now - state.lastReload;
-    const reloadTime = Math.min(timeDelta, interval);
-    const reloadedTurns = reloadTime * throughput;
-    const newAvalableTurns = state.availableTurns + reloadedTurns;
-
-    state.lastReload = now;
-    state.availableTurns = Math.min(newAvalableTurns, turnsPerInterval);
-  }
 
   async function waitTurn(turns?: number = 1): Promise<void> {
     if (turns > turnsPerInterval) {
       throw new Error('Turns can not be greater than the number of turns per interval');
     }
 
-    reload();
+    state = reload(turnsPerInterval, interval, state);
 
     const turnsToWait = Math.max(0, turns - state.availableTurns);
     const wait = Math.ceil(turnsToWait * timePerTurn);
