2e063a7ffd88be0cca8545dcac16f66219047a90
Emily Marigold Klassen <forivall@gmail.com>
2020-08-15 17:57:48 -0700

    test: add forgotten test run

--- test/wyt.js
+++ test/wyt.js
@@ -150,7 +150,7 @@ test('throw if taking more turns per take than turnsperInterval', async (t) => {
   await t.throwsAsync(() => waitTurn(3));
 });
 
-test('2 per second in parallel, after different timeoutss', async (t) => {
+test('2 per second in parallel, after different timeouts', async (t) => {
   const rpi = 2;
   const interval = 1000;
   const waitTurn = wyt(rpi, interval);
@@ -182,4 +182,5 @@ test('2 per second in parallel, after different timeoutss', async (t) => {
   await batch();
   // ensure that wyt won't next try to overprocess after a long wait
   await new Promise((resolve) => setTimeout(resolve, 3500));
+  await batch();
 });
